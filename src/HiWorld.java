fa4a69249c36a50b8fabde5c72a0c8ddab99dd40
Beacon <1533928999@qq.com>
2020-02-08 11:27:33 +0800

    add sout beacon come on add 666

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -3,7 +3,7 @@ public class HiWorld {
         System.out.println("hi world");
         System.out.println("Beacon come on");
         System.out.println(666);
-        System.out.println(11);
+        System.out.println(111);
 
     }
 }
